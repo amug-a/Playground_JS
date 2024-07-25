2199c7a63ea7585ac85fcafa3bd3f9788fcdea98
amug-a <65325991+amug-a@users.noreply.github.com>
2024-07-25 10:03:46 +0100

    refactor index.js and configured gulp.js

--- config/gulp.js
+++ config/gulp.js
@@ -1,3 +1,5 @@
+const browserSync = require("browser-sync");
+
 var path = {
   // Declaring our distribution folder
   dist_dir: 'dist',
@@ -5,6 +7,28 @@ var path = {
   dist_files: 'dist/**/*.*',
 
   views: {
-    src: 'src/views/**/*.ejs'
+    src: 'src/views/**/*.ejs',
+    dist: 'dist/view'
+  },
+  styles: {
+    src: 'src/styles/**/*.css',
+    dist: 'dist/public/styles'
   }
-}
+};
+
+modules.exports = {
+  paths: paths,
+  plugins: {
+    browserSync: {
+      proxy: "localhost:300",
+      port: 5000,
+      files: [paths.dist_files],
+      browser: 'google chrome',
+      notify: true
+    },
+    nodemone: {
+      script: 'index.js',
+      ignore: ['gulpfile.js', 'config/', 'node_modules/']
+    }
+  }
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,45 @@
-var gulp = require('gulp');
+const { watch } = require('browser-sync');
+
+var gulp = require('gulp'),
+  config = require('./config/gulp'),
+  nodemone = require('gulp-nodemon'),
+  del = require('del');
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
+
+
+// gulp task to clear the dit directory
+gulp.task('clean', function () {
+  return del(config.paths.dist_dir);
+})
+
+// gulp task to watch the views directory
+gulp.task('dev:views', function () {
+  return gulp.src(config.paths.views.src)/* To process the views */.pipe(gulp.dest(config.paths.views.dist))
+})
+
+// gulp task to watch for styles in the styles directory
+gulp.task('watch:styles', function () {
+  return gulp.src(config.paths.styles.dist)/* To process the styles*/.pipe(gulp.dest(config.paths.styles.dist))
+})
+
+// gulp task to watch for scripts in the scripts directory
+gulp.task('watch:styles', function () {
+  gulp.watch(config.paths.styles.src, gulp.series('dev:styles'));
+  done();
+})
+
+// gulp task to serve the files
+gulp.task('server', function () {
+  var called = false;
+  return nodemon(config.plugins.nodemon).on('start', function (serve) {
+    if (!called) {
+      called = true;
+      console.log('Server started....');
+      serve();
+    }
+  })
+});
 
 gulp.task('default', function () {
   browserSync.init({
