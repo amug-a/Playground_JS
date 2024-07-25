64718f83fbcebb8d8d9142c3864a0d3b8e5935fe
amug-a <65325991+amug-a@users.noreply.github.com>
2024-07-25 09:26:06 +0100

    hello hello_world

---
+++ config/gulp.js
@@ -0,0 +1,10 @@
+var path = {
+  // Declaring our distribution folder
+  dist_dir: 'dist',
+  // Declaring all files in our dist folder as our dist files
+  dist_files: 'dist/**/*.*',
+
+  views: {
+    src: 'src/views/**/*.ejs'
+  }
+}
